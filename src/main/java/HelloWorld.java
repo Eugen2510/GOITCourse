b6b72b543bcd7ab0fce0017d2f44cff131447cdd
Eugene <lugovoj20@gmail.com>
2023-02-02 18:05:24 +0200

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        /**
+         * Just for myself
+         */
+
+        /*
+        Here is different color of text)
+         */
+        System.out.println("Hello world!");
+    }
+}
